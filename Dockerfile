a219a3ad05e411f596de20426365d2c68f236dc7
wikiselev <vk6@sanger.ac.uk>
2018-09-19 18:52:03 +0100

    first commit

---
+++ Dockerfile
@@ -0,0 +1,50 @@
+FROM rocker/rstudio:3.5.0
+
+# install some R required stuff
+RUN apt-get update -y --no-install-recommends \
+    && apt-get -y install -f \
+       zlib1g-dev \
+       libssl-dev \
+       libcurl4-openssl-dev
+
+# R packages
+# https://askubuntu.com/questions/610449/w-gpg-error-the-following-signatures-couldnt-be-verified-because-the-public-k
+RUN apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
+# https://cran.r-project.org/bin/linux/ubuntu/README.html
+RUN echo "deb https://cloud.r-project.org/bin/linux/ubuntu bionic-cran35/" | sudo tee -a /etc/apt/sources.list
+# https://launchpad.net/~marutter/+archive/ubuntu/c2d4u3.5
+RUN add-apt-repository ppa:marutter/c2d4u3.5
+# Install CRAN binaries from ubuntu
+RUN apt-get update && apt-get install -yq --no-install-recommends \
+    r-cran-data.table \
+    r-cran-DT \
+    r-cran-devtools \
+    r-cran-ggplot2 \
+    && apt-get clean && \
+    rm -rf /var/lib/apt/lists/*
+
+RUN echo 'source("https://bioconductor.org/biocLite.R")' > /opt/bioconductor.r && \
+    echo 'biocLite()' >> /opt/bioconductor.r && \
+    echo 'biocLite(c("SingleCellExperiment", "SummarizedExperiment"))' >> /opt/bioconductor.r && \
+    Rscript /opt/bioconductor.r
+
+RUN Rscript -e "devtools::install_github('pati-ni/scfind', ref='develop')"
+
+# install shiny
+RUN export ADD=shiny && bash /etc/cont-init.d/add
+
+# add app to the server
+ADD indexes indexes/
+ADD app app/
+RUN for d in indexes/*/; do cp app/* "$d"; done
+RUN cp -r indexes/* /srv/shiny-server
+
+# update the index page
+COPY index_page/index.html /srv/shiny-server/index.html
+COPY index_page/img /srv/shiny-server/img
+
+# try to avoid greying out of the apps
+# https://stackoverflow.com/questions/44397818/shiny-apps-greyed-out-nginx-proxy-over-ssl
+RUN echo 'sanitize_errors off;disable_protocols xdr-streaming xhr-streaming iframe-eventsource iframe-htmlfile;' >> /etc/shiny-server/shiny-server.conf
+
+CMD ["/init"]
--- app/server.R
+++ app/server.R
@@ -3,142 +3,128 @@ library(scfind)
 library(shiny)
 library(DT)
 library(data.table)
-library(ggplot)
+library(ggplot2)
 
-## Global variables ?? not sure
-values <-  reactiveValues()
+options(shiny.maxRequestSize=200*1024^2)
 
-server.scfind <- function(object)
+# A read-only data set that will load once, when Shiny starts, and will be
+# available to each user session
+object <- loadObject("www/mca.rds")
+
+server <- function(input, output, session)
 {
+    gene.list <- reactive({
+        text <- gsub("\\s", "", input$geneList)
+        gene.list.input <- unlist(strsplit(text, ","))
+        print(gene.list.input)
+        gene.list.input
+    })
 
-    return(
-        function(input, output, session)
+    checkbox.selection <- reactive({
+        
+        selected.index <- input$queryOptimizer_rows_selected
+        if (!is.null(selected.index))
         {
-            
+            available.queries <-  recommended.queries()
+            selected.query <- available.queries[selected.index, 'Query']
+            print(paste0('selected query', selected.query))
+            genes <-  unlist(strsplit(gsub("\\s", "", selected.query), ","))
+        }
+        else
+        {
+            if (is.null(input$geneCheckbox))
+            {
+                genes <- gene.list()
+            }
+            else
+            {
+                genes <- input$geneCheckbox
+            }
+        }
+        print(genes)
+        genes
+        ## updateSelectInput(session, "geneCheckbox", selected  = genes)
+    })
 
-            gene.list <- reactive({
-                text <- gsub("\\s", "", input$geneList)
-                gene.list.input <- unlist(strsplit(text, ","))
-                print(gene.list.input)
-                gene.list.input
-            })
-
-            checkbox.selection <- reactive({
-                
-                selected.index <- input$queryOptimizer_rows_selected
-                if (!is.null(selected.index))
-                {
-                    available.queries <-  recommended.queries()
-                    selected.query <- available.queries[selected.index, 'Query']
-                    print(paste0('selected query', selected.query))
-                    genes <-  unlist(strsplit(gsub("\\s", "", selected.query), ","))
-                }
-                else
-                {
-                    if (is.null(input$geneCheckbox))
-                    {
-                        genes <- gene.list()
-                    }
-                    else
-                    {
-                        genes <- input$geneCheckbox
-                    }
-                }
-                print(genes)
-                genes
-                ## updateSelectInput(session, "geneCheckbox", selected  = genes)
-            })
-
-            
-            output$geneCheckbox <- renderUI({
-                
-                ## Select genes
-                checkboxGroupInput("geneCheckbox", h4("Select Genes"), choices = gene.list(), selected = checkbox.selection(), inline = T)
-            })
-           
-            
-            
-            
-            recommended.queries <- reactive({
-                
-                selected.genes <- gene.list()
-                selected.datasets <- input$datasetCheckbox
-                if (length(selected.genes) != 0)
-                {
-                    available.queries <-  markerGenes(object, selected.genes, selected.datasets)
-                }
-                else
-                {
-                    available.queries <- c()
-                }
-                available.queries <- as.data.table(available.queries)
-                available.queries
-            })
+    output$geneCheckbox <- renderUI({
+        
+        ## Select genes
+        checkboxGroupInput("geneCheckbox", h4("Select Genes"), choices = gene.list(), selected = checkbox.selection(), inline = T)
+    })
+    
+    recommended.queries <- reactive({
+        
+        selected.genes <- gene.list()
+        selected.datasets <- input$datasetCheckbox
+        if (length(selected.genes) != 0)
+        {
+            available.queries <-  markerGenes(object, selected.genes, selected.datasets)
+        }
+        else
+        {
+            available.queries <- c()
+        }
+        available.queries <- as.data.table(available.queries)
+        available.queries
+    })
 
 
-            output$queryOptimizer <- renderDataTable({
-                
-                datatable(recommended.queries(), selection = 'single')
-            })
+    output$queryOptimizer <- renderDataTable({
+        
+        datatable(recommended.queries(), selection = 'single')
+    })
 
-            
-            
-            cell.types <- reactive({
-                selection <- checkbox.selection()
-                
-                if (length(selection) != 0){
-                    df <- query.result.as.dataframe(findCellTypes(object, selection, input$datasetCheckbox))
-                    df
-                }
-                else
-                {
-                    data.frame(cell_type = c(), cell_id = c())
-                }
-            })
+    cell.types <- reactive({
+        selection <- checkbox.selection()
+        
+        if (length(selection) != 0){
+            df <- query.result.as.dataframe(findCellTypes(object, selection, input$datasetCheckbox))
+            df
+        }
+        else
+        {
+            data.frame(cell_type = c(), cell_id = c())
+        }
+    })
 
-            gene.support <- reactive({
-                gene.selection <- gene.list()
-                dataset.selection <- input$datasetCheckbox
-                gene.support <- as.data.frame(object@index$genesSupport(gene.selection, dataset.selection))
-                dimnames(gene.support)[[2]] <- 'support'
-                gene.support$genes <- rownames(gene.support)
-                gene.support
-            })
-            
-            
-            output$cellTypesData <- renderDataTable({                
-                df <- cell.types()
-                datatable(phyper.test(object, df, input$datasetCheckbox), selection = 'single')
-                
-            })
-            
-            output$geneSupportHisto <- renderPlot({
-                ## Render a barplot
-                ## print(length(input$geneCheckbox))
-                ## print(input$geneCheckbox)
-                df <- gene.support()
-                print(df)
-                if (nrow(df) != 0)
-                {
-                    g <- ggplot(df, aes(x=genes, y= support)) +
-                        xlab("Gene") +
-                        ylab("Cells") +
-                        geom_col(color = "blue") +
-                        coord_flip() +
-                        theme_minimal()
-                }
-                else
-                {
-                    g <- plot(0,type='n',axes=FALSE,ann=FALSE)
-                }
-                g
-            })
+    gene.support <- reactive({
+        gene.selection <- gene.list()
+        dataset.selection <- input$datasetCheckbox
+        gene.support <- as.data.frame(object@index$genesSupport(gene.selection, dataset.selection))
+        dimnames(gene.support)[[2]] <- 'support'
+        gene.support$genes <- rownames(gene.support)
+        gene.support
+    })
+    
+    output$cellTypesData <- renderDataTable({                
+        df <- cell.types()
+        datatable(phyper.test(object, df, input$datasetCheckbox), selection = 'single')
+        
+    })
+    
+    output$geneSupportHisto <- renderPlot({
+        ## Render a barplot
+        ## print(length(input$geneCheckbox))
+        ## print(input$geneCheckbox)
+        df <- gene.support()
+        print(df)
+        if (nrow(df) != 0)
+        {
+            g <- ggplot(df, aes(x=genes, y= support)) +
+                xlab("Gene") +
+                ylab("Cells") +
+                geom_col(color = "blue") +
+                coord_flip() +
+                theme_minimal()
+        }
+        else
+        {
+            g <- plot(0,type='n',axes=FALSE,ann=FALSE)
+        }
+        g
+    })
 
-            
-            
-            
-            session$onSessionEnded(function() {
-                stopApp()
-            })
-        })
+    session$onSessionEnded(function() {
+        stopApp()
+    })
 }
